af3c3e93f335ba61bc26542f7dd34567fb9ba199
shahidshaikh077 <shahidahamadshaikh07@gmail.com>
2026-09-09 13:17:14 +0530

    Add files via upload

---
+++ movie.pl
@@ -0,0 +1,19 @@
+likes(shahid,action).
+likes(gururaj,advanture).
+likes(yash,emotional).
+likes(azar,romance).
+likes(kunal,comedy).
+
+
+movie(jawan,action).
+movie(jumangi,advanture).
+movie(veer zaara,emotional).
+movie(ashiqui,romance).
+movie(hera_pheri,comedy).
+
+
+#rules
+
+recommend(user,movie):-
+movie(movie,genre);
+likes(movie,user).
